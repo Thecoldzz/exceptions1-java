1c268f41efbde9a71c11dd4afcddd1e49ccacb7b
Lucas Pedrosa <eokasemytrout@gmail.com>
2024-09-23 10:00:57 -0300

    Very bad solution

---
+++ main/Program.java
@@ -0,0 +1,52 @@
+package excecoes.exemplo02.main;
+
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.Scanner;
+
+import excecoes.exemplo02.modelentities.Reservation;
+
+import java.util.Date;
+
+public class Program {
+    public static void main(String [] Args) throws ParseException{
+        Scanner sc = new Scanner(System.in);
+        SimpleDateFormat sdf = new SimpleDateFormat("dd/MM/yyyy");
+
+        System.out.print("Room number: ");
+        int num = sc.nextInt();
+        System.out.print("Check-in date: (dd/mm/yyyy)");
+        Date checkIn = sdf.parse(sc.next());
+        System.out.print("Check-out date: (dd/mm/yyyy)");
+        Date checkOut = sdf.parse(sc.next());
+        if(!checkOut.after(checkIn)){ // se lê : ! = não. Se checkout não for depois de checkin faça
+            System.out.print("Check-out date must be after check-in date!");
+        }else{
+            Reservation res = new Reservation(num,checkIn,checkOut);
+            System.out.println(res);
+
+            System.out.println("Enter data to update reservation: ");
+            
+            
+            System.out.print("Check-in date: (dd/mm/yyyy)");
+            checkIn = sdf.parse(sc.next());
+            System.out.print("Check-out date: (dd/mm/yyyy)");
+            checkOut = sdf.parse(sc.next());
+            Date now = new Date();
+            if(checkIn.before(now) || checkOut.before(now)){
+                System.out.println("Error in reserve! Reservation dates must be future dates!");
+            }
+            else if(!checkOut.after(checkIn)){
+                System.out.print("Check-out date must be after check-in date!");
+            }
+            else{
+            res.updateDates(checkIn, checkOut);
+            System.out.println(res);
+            }
+        }
+
+
+        
+        sc.close();
+    }
+}
---
+++ modelentities/Reservation.java
@@ -0,0 +1,45 @@
+package excecoes.exemplo02.modelentities;
+
+import java.text.SimpleDateFormat;
+import java.util.Date;
+import java.util.concurrent.TimeUnit;
+
+public class Reservation {
+    private Integer roomNumber;
+    private Date checkIn;
+    private Date checkOut;
+    private static SimpleDateFormat sdf = new SimpleDateFormat("dd/MM/yyyy");
+
+    public Reservation(){
+    }
+    public Reservation(Integer roomNumber, Date checkIn, Date checkOut) {
+        this.roomNumber = roomNumber;
+        this.checkIn = checkIn;
+        this.checkOut = checkOut;
+    }
+    public Integer getRoomNumber() {
+        return roomNumber;
+    }
+    public void setRoomNumber(Integer roomNumber) {
+        this.roomNumber = roomNumber;
+    }
+    public Date getCheckIn() {
+        return checkIn;
+    }
+    public Date getCheckOut() {
+        return checkOut;
+    }
+    public long duration(){
+        long diff = checkOut.getTime() - checkIn.getTime(); // pega a diferença em milisegundos do checkin e checkout, ou seja o dia q chegou e o dia que vai sair.
+        return TimeUnit.DAYS.convert(diff, TimeUnit.MILLISECONDS);
+    }
+    public void updateDates(Date checkIn, Date checkOut){ // passa uma data pra atualizar e atualiza a do obejto, o this.checkin é do obj e o checkIn é o valor passado.
+        this.checkIn = checkIn;
+        this.checkOut = checkOut;
+    }
+    
+    @Override
+    public String toString(){
+        return "Room: " + roomNumber + " check-in: " + sdf.format(checkIn) + ", check-out: " + sdf.format(checkOut) + ", " + duration() + " nights.";
+    }
+}
